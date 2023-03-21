48707f1d7800d82666bdc3eaa7c49799a9acf5f1
PiotrCiba <piotr.m.ciba@gmail.com>
2023-03-21 23:02:16 +0100

    GUI update: Binding the selectable values to the combo boxes. TODO: Layout update method, Datagrids Pokolenia & Chromosomy

--- KlawiaturaAG/MainWindow.xaml.cs
+++ KlawiaturaAG/MainWindow.xaml.cs
@@ -8,6 +8,15 @@ namespace KlawiaturaAG
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string[] Layouts { get; set; } = { "QWERTY", "Dvorak", "ARENSITO", "Colemak", "<Selected>" };
+        public int[] ParentValues { get; set; } = { 1, 2 };
+        public int[] ChildrenValues { get; set; } = { 1, 2 };
+        public string[] SelectionAlgorithms { get; set; } = {"Turniej", "Ruletka"};
+        public string[] CrossoverAlgorithms { get; set; } = { "OX", "ERX" };
+        public string[] MutationAlgorithms { get; set; } = { "Pair Swap", "Partial Scramble" };
+        public string[] SeverityTypes { get; set; } = {"Random", "Continuous", "Spread" };
+
+
         public MainWindow()
         {
             InitializeComponent();
